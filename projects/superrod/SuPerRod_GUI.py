fix issue of weighting not taking effect during fit in superrod

projects/superrod/SuPerRod_GUI.py
@@ -602,7 +602,7 @@ class MyMainWindow(QMainWindow):
                         rod_factor = getattr(self.model.script_module.rgh, extra_scale_factor)
                     else:
                         rod_factor = 1
-                    self.data_profiles[i].plot(self.model.data[i+offset].x, self.f_ideal[i+offset]*(scale_factor*rod_factor)**2,pen = {'color': "w", 'width': 1},clear = False)
+                    self.data_profiles[i].plot(self.model.data[i+offset].x, self.f_ideal[i+offset]*scale_factor*rod_factor,pen = {'color': "w", 'width': 1},clear = False)
                 except:
                     pass
                 #plot simulated results
@@ -1491,8 +1491,14 @@ class MyMainWindow(QMainWindow):
                 beta = self.model.script_module.rgh.beta
                 #rough = (1-beta)/((1-beta)**2 + 4*beta*np.sin(np.pi*(each.x-each.extra_data['LB'])/each.extra_data['dL'])**2)**0.5
                 scale_factor = [self.model.script_module.rgh.scale_nonspecular_rods,self.model.script_module.rgh.scale_specular_rod][int("00L" in each.name)]
+                h_, k_ = int(round(each.extra_data['h'][0],0)),int(round(each.extra_data['k'][0],0))
+                extra_scale_factor = 'scale_factor_{}{}L'.format(h_,k_)
+                if hasattr(self.model.script_module.rgh,extra_scale_factor):
+                    rod_factor = getattr(self.model.script_module.rgh, extra_scale_factor)
+                else:
+                    rod_factor = 1
                 rough = 1
-                export_data['I_bulk'] = np.append(export_data['I_bulk'],rough**2*np.array(self.model.script_module.sample.calc_f_ideal(each.extra_data['h'], each.extra_data['k'], each.x)**2*scale_factor**2))
+                export_data['I_bulk'] = np.append(export_data['I_bulk'],rough**2*np.array(self.model.script_module.sample.calc_f_ideal(each.extra_data['h'], each.extra_data['k'], each.x)**2*scale_factor*rod_factor))
             df_export_data = pd.DataFrame(export_data)
             writer_temp = pd.ExcelWriter([path+'.xlsx',path][int(path.endswith('.xlsx'))])
             df_export_data.to_excel(writer_temp, columns =['potential']+[lib_map[each_] for each_ in ['x','h','k','y','y_sim','error']]+['I_bulk','use'])

projects/superrod/standard_scripts/model_script_standard_Cu_new.py
@@ -161,13 +161,16 @@ def Sim(data,VARS=vars()):
     total_wt = sum(wt_list)
     for i in range(num_surface_slabs):
         sample.domain['domain{}'.format(i+1)]['wt']=wt_list[i]/total_wt
-    
-    #update sorbate symmetry(not necessary)
+
+    #faster solution(a factor of two faster than using loop)
     '''
-    for i in range(num_sorbate_slabs):
-        for each in VARS['sorbate_syms_{}'.format(i+1)]:
-            for n in range(num_surface_slabs):
-                each.set_t([VARS['atm_gp_surface_1st_layer_{}'.format(n+1)].getdx(),VARS['atm_gp_surface_1st_layer_{}'.format(n+1)].getdy()]) # models.structure_tools.sxrd_dafy.AtomGroup
+    h_, k_, x_,LB_,dL_ = data.ctr_data_all[:,0], data.ctr_data_all[:,1], data.ctr_data_all[:,2],data.ctr_data_all[:,4],data.ctr_data_all[:,5]
+    rough_ = (1-beta)/((1-beta)**2 + 4*beta*np.sin(np.pi*(x_-LB_)/dL_)**2)**0.5
+    f_ = rough_*sample.calc_f_all(h_, k_, x_)
+    F_ = abs(f_*f_)*rgh.scale_nonspecular_rods
+    #you need to edit the list of extra scaling factor accordingly
+    F = data.split_fullset(F_,[rgh.scale_factor_11L,rgh.scale_factor_20L,rgh.scale_factor_22L,rgh.scale_factor_31L])
+    fom_scaler = [1]*len(F)
     '''
 
     for data_set in data:
@@ -189,15 +192,17 @@ def Sim(data,VARS=vars()):
                 if h[0]==0 and k[0]==0:#consider layered water only for specular rod if existent
                     q=np.pi*2*unitcell.abs_hkl(h,k,x)
                     pre_factor=(np.exp(-exp_const*rgh.mu/q))*(4*np.pi*re/auc)*3e6
-                    f = rgh.scale_specular_rod*rough*sample.calc_f_all(h, k, x)
+                    f = rgh.rough*sample.calc_f_all(h, k, x)
+                    F.append(abs(f*f)*rgh.scale_specular_rod)
                 else:
-                    f = rough*rgh.scale_nonspecular_rods*sample.calc_f_all(h, k, x)
-                F.append(abs(f*f))
+                    f = rough*sample.calc_f_all(h, k, x)
+                    scale_factor_extra = getattr(rgh,'scale_factor_{}{}L'.format(int(round(h[0],0)),int(round(k[0],0))))
+                    F.append(abs(f*f)*rgh.scale_nonspecular_rods*scale_factor_extra)
                 fom_scaler.append(1)
         else:
             f=np.zeros(len(y))
             F.append(f)
             fom_scaler.append(1)
 
-    panelty_factor = [sample.bond_distance_constraint(which_domain=i, max_distance =2.2) for i in range(num_surface_slabs)]
+    panelty_factor = [sample.bond_distance_constraint(which_domain=i, max_distance =1) for i in range(num_surface_slabs)]
     return F,1+sum(panelty_factor),fom_scaler

util/fom_funcs.py
@@ -187,50 +187,46 @@ bg_peaks={'00':[0,2,4,6],'02':[-8.2782,-6.2782,-4.2782,-2.2782,-0.2782,1.7218,3.
 #==============================================================================
 # BEGIN FOM function defintions
 
+#decorator to weight FOM (will be imported in the model, and the decoration will be done at site)
+def weight_fom_based_on_HKL(weight_factor,weight_map):
+    #eg weight_factor = 10
+    #eg weight_map = {(1,1):[[1.6,2.4],[3.6,4.4]]}, which define the l segments to be weighted
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args,**kwargs):
+            # print(kwargs.keys(),len(args))
+            if len(kwargs) == 0:
+                datasets = args[1]
+            else:
+                datasets = kwargs['data']
+            weight_array_list = []
+            for dataset in datasets:
+                hk_tag = (int(round(dataset.extra_data['h'][0],0)), int(round(dataset.extra_data['k'][0],0)))
+                # if hk_tag in weight_pars.weight_map:
+                if hk_tag in weight_map:
+                    # L_segments = weight_pars.weight_map[hk_tag]
+                    L_segments = weight_map[hk_tag]
+                    l = dataset.x
+                    conditions_str_list = []
+                    for each in L_segments:
+                        conditions_str_list.append('((l>={}) & (l<={}))'.format(*each))
+                    condition = eval('|'.join(conditions_str_list))
+                    coniditon_false = (condition==False)*1
+                    # condition_true = condition*1*weight_pars.weight_factor
+                    condition_true = condition*1*weight_factor
+                    weight_array_list.append(condition_true + coniditon_false)
+                else:
+                    weight_array_list.append(np.ones(len(dataset.x)))
+            results = func(*args,**kwargs)
+            for i, weight_array in enumerate(weight_array_list):
+                results[i] = results[i]*weight_array
+            return results
+        return wrapper
+    return decorator
+
 #=========================
 # unweighted FOM functions
-
-class weight_pars(object):
-    weight_factor = 1
-    #each_key is a tuple of (h,k)
-    #each item value is a list of two-item list, each two-item list define the l segment where the weighting will be taken effect
-    #you can difein multiple l segments
-    weight_map = {(1,1):[[1.6,2.4]],
-                  (2,0):[[0.8,1.4],[2.5,3.5]],
-                  (2,2):[[1,1.4],[2.5,3.5]],
-                  (3,1):[[1.5,2.5]]}
-
-#decorator to weight FOM
-def weight_fom_based_on_HKL(func):
-    @functools.wraps(func)
-    def wrapper(*args,**kwargs):
-        # print(kwargs.keys(),len(args))
-        if len(kwargs) == 0:
-            datasets = args[1]
-        else:
-            datasets = kwargs['data']
-        weight_array_list = []
-        for dataset in datasets:
-            hk_tag = (int(round(dataset.extra_data['h'][0],0)), int(round(dataset.extra_data['k'][0],0)))
-            if hk_tag in weight_pars.weight_map:
-                L_segments = weight_pars.weight_map[hk_tag]
-                l = dataset.x
-                conditions_str_list = []
-                for each in L_segments:
-                    conditions_str_list.append('((l>={}) & (l<={}))'.format(*each))
-                condition = eval('|'.join(conditions_str_list))
-                coniditon_false = (condition==False)*1
-                condition_true = condition*1*weight_pars.weight_factor
-                weight_array_list.append(condition_true + coniditon_false)
-            else:
-                weight_array_list.append(np.ones(len(dataset.x)))
-        results = func(*args,**kwargs)
-        for i, weight_array in enumerate(weight_array_list):
-            results[i] = results[i]*weight_array
-        return results
-    return wrapper
-
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def diff(simulations, data):
     ''' Average absolute difference
     '''
@@ -242,7 +238,7 @@ def diff(simulations, data):
 diff.__div_dof__ = True
 
 #decorator func for Fom weighting purpose
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def log(simulations, data):
     ''' Average absolute logartihmic difference
     '''
@@ -251,7 +247,7 @@ def log(simulations, data):
         for (dataset, sim) in zip(data,simulations)]
 log.__div_dof__ = True
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def log_debug(simulations, data):
     ''' Average absolute logartihmic difference
     '''
@@ -264,7 +260,7 @@ def log_debug(simulations, data):
         for (dataset, sim) in zip(data,simulations)]
 log_debug.__div_dof__ = True
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def sqrt(simulations, data):
     ''' Average absolute difference of the square root
     '''
@@ -273,7 +269,7 @@ def sqrt(simulations, data):
         for (dataset, sim) in zip(data,simulations)]
 sqrt.__div_dof__ = True
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def R1(simulations, data):
     ''' Crystallographic R-factor (R1)
     '''
@@ -282,7 +278,7 @@ def R1(simulations, data):
     return [1.0/denom*(np.sqrt(np.abs(dataset.y)) - np.sqrt(np.abs(sim)))\
         for (dataset, sim) in zip(data,simulations)]
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def R1_weighted(simulations, data):
     ''' Crystallographic R-factor (R1)
     '''
@@ -291,7 +287,7 @@ def R1_weighted(simulations, data):
     return [1.0/denom*abs(np.sqrt(np.abs(dataset.y)) - np.sqrt(np.abs(sim)))/np.sqrt(np.abs(dataset.y))\
         for (dataset, sim) in zip(data,simulations)]
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def R1_weighted_2(simulations, data):
     ''' Crystallographic R-factor (R1)
     '''
@@ -308,7 +304,7 @@ def R1_weighted_2(simulations, data):
     return return_list
 
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def chi2bars_2(simulations, data):
     ''' Weighted chi squared
     '''
@@ -323,7 +319,7 @@ def chi2bars_2(simulations, data):
     return return_list
 chi2bars_2.__div_dof__ = True
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def R1_weighted_2b(simulations, data):
         ''' Crystallographic R-factor (R1)
         '''
@@ -339,7 +335,7 @@ def R1_weighted_2b(simulations, data):
                 return_list.append(1.0/denom*abs(np.sqrt(np.abs(dataset.y)) - np.sqrt(np.abs(sim)))/np.sqrt(np.abs(dataset.y)))
         return return_list
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def R1_weighted_3(simulations, data):
     ''' Crystallographic R-factor (R1)
     '''
@@ -355,7 +351,7 @@ def R1_weighted_3(simulations, data):
             return_list.append(1.0/denom*abs(np.log10(np.sqrt(np.abs(dataset.y))) - np.log10(np.sqrt(np.abs(sim)))))
     return return_list
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def logR1(simulations, data):
     ''' logarithmic crystallographic R-factor (R1)
     '''
@@ -365,7 +361,7 @@ def logR1(simulations, data):
                                         np.log10(np.sqrt(sim)))\
         for (dataset, sim) in zip(data,simulations)]
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def R2(simulations, data):
     ''' Crystallographic R2 factor
     '''
@@ -374,7 +370,7 @@ def R2(simulations, data):
     return [1.0/denom*np.sign(dataset.y - sim)*(dataset.y - sim)**2\
         for (dataset, sim) in zip(data,simulations)]
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def R2_weighted(simulations, data):
     ''' Crystallographic R2 factor
     '''
@@ -384,7 +380,7 @@ def R2_weighted(simulations, data):
         for (dataset, sim) in zip(data,simulations)]
 
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def logR2(simulations, data):
     ''' logarithmic crystallographic R2 factor
     '''
@@ -393,7 +389,7 @@ def logR2(simulations, data):
     return [1.0/denom*np.sign(np.log10(dataset.y) - np.log10(sim))*(np.log10(dataset.y) - np.log10(sim))**2\
         for (dataset, sim) in zip(data,simulations)]
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def sintth4(simulations, data):
     ''' Sin(tth)^4 scaling of the average absolute difference for reflectivity.
     '''
@@ -403,7 +399,7 @@ def sintth4(simulations, data):
         for (dataset, sim) in zip(data,simulations)]
 sintth4.__div_dof__ = True
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def Norm(simulations, data):
     '''  dataset normalized 1/3 scaling of the error
     '''
@@ -414,7 +410,7 @@ Norm.__div_dof__ = True
 #=======================
 # weighted FOM functions
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def chi2bars(simulations, data):
     ''' Weighted chi squared
     '''
@@ -422,7 +418,7 @@ def chi2bars(simulations, data):
     return [(dataset.y - sim)**2/dataset.error**2 for (dataset, sim) in zip(data,simulations)]
 chi2bars.__div_dof__ = True
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def chi2bars_one_time(simulations, data):
     ''' Weighted chi squared
     '''
@@ -430,7 +426,7 @@ def chi2bars_one_time(simulations, data):
     return [(dataset.y - sim)**2/(dataset.error+4.55+dataset.y*0.03)**2 for (dataset, sim) in zip(data,simulations)]
 chi2bars_one_time.__div_dof__ = True
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def chi2bars_w_trainor(simulations, data):
     ''' Weighted chi squared
     '''
@@ -439,7 +435,7 @@ def chi2bars_w_trainor(simulations, data):
 chi2bars_w_trainor.__div_dof__ = True
 
 #fom's are weighted with dip zones having higher wt number and bragg peak zone having lower wt number
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def chi2bars_weighted(simulations, data):
     ''' Weighted chi squared
     '''
@@ -466,7 +462,7 @@ def chi2bars_weighted(simulations, data):
 chi2bars_weighted.__div_dof__ = True
 
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def chibars(simulations, data):
     ''' Weighted chi squared but without the squaring
     '''
@@ -475,7 +471,7 @@ def chibars(simulations, data):
         for (dataset, sim) in zip(data,simulations)]
 chibars.__div_dof__ = True
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def logbars(simulations, data):
     ''' Weighted average absolute difference of the logarithm of the data
     '''
@@ -485,7 +481,7 @@ def logbars(simulations, data):
         for (dataset, sim) in zip(data,simulations)]
 logbars.__div_dof__ = True
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def R1bars(simulations, data):
     ''' Weighted crystallographic R-factor (R1)
     '''
@@ -495,7 +491,7 @@ def R1bars(simulations, data):
            (np.sqrt(dataset.y) - np.sqrt(sim))
         for (dataset, sim) in zip(data,simulations)]
 
-@weight_fom_based_on_HKL
+#@weight_fom_based_on_HKL
 def R2bars(simulations, data):
     ''' Weighted crystallographic R2 factor
     '''