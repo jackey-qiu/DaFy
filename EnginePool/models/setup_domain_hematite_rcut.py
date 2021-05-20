fix a bug due to wrong scaling factor applied to f_ideal

EnginePool/models/setup_domain_hematite_rcut.py
@@ -320,10 +320,10 @@ def setup_raxr_pars_new(NUMBER_SPECTRA,F1F2_FILE):
         F1F2=np.loadtxt(os.path.join(F1F2_FILE))
         rgh_raxr=UserVars()
         for i in range(NUMBER_SPECTRA):
-            rgh_raxr.new_var('a'+str(i+1),0.0)
-            rgh_raxr.new_var('b'+str(i+1),0.0)
-            rgh_raxr.new_var('c'+str(i+1),0.0)
-            rgh_raxr.new_var('A_'+str(i+1),2.0)
+            rgh_raxr.new_var('a'+str(i+1),1.0)
+            rgh_raxr.new_var('b'+str(i+1),1.0)
+            rgh_raxr.new_var('c'+str(i+1),1.0)
+            rgh_raxr.new_var('A_'+str(i+1),0.2)
             rgh_raxr.new_var('P_'+str(i+1),0.0) 
     else:
         rgh_raxr=None

projects/superrod/standard_scripts/template_script_hematite_rcut.py
@@ -10,7 +10,7 @@ from models.structure_tools import sorbate_tool_beta as sorbate_tool
 
 import models.setup_domain_hematite_rcut as setup_domain_hematite_rcut
 from accessory_functions.data_formating.data_formating import format_hkl
-from UtilityFunctions import config_file_parser_bv, update_O_NUMBER
+from UtilityFunctions import config_file_parser_bv, update_O_NUMBER, setup_raxr_pars_new
 from FitEnginePool import bond_valence_constraint
 
 model_type = 'ctr'
@@ -23,7 +23,7 @@ E0=11873
 F1F2_FILE='As_K_edge_March28_2018.f1f2'
 #/raxs/end#
 if NUMBER_SPECTRA!=0:
-    rgh_raxs,F1F2=setup_domain_hematite_rcut.setup_raxr_pars_new(NUMBER_SPECTRA, F1F2_FILE)
+    rgh_raxs,F1F2 = setup_raxr_pars_new(NUMBER_SPECTRA, F1F2_FILE)
 else:
     rgh_raxs, F1F2 = None, []
 
@@ -235,8 +235,8 @@ def Sim(data,VARS=vars()):
         sub_sets, data_info = data.split_used_dataset(F_, data_type = 'RAXS')
         F_ = data.insert_datasets(full_set = F_all, sub_sets = sub_sets, data_info = data_info, data_type = 'RAXS')
         #you need to edit the list of extra scaling factor accordingly
-        #scaling_factors = [[rgh.scale_nonspecular_rods, rgh.scale_specular_rod][int(each=='specular_rod')] for each in data.scaling_tag]
-        F_raxs = data.split_fullset(F_,scale_factors=1, data_type = 'RAXS')
+        scaling_factors = [[rgh.scale_nonspecular_rods, rgh.scale_specular_rod][int(each=='specular_rod')] for each in data.scaling_tag_raxs]
+        F_raxs = data.split_fullset(F_,scale_factors=scaling_factors, data_type = 'RAXS')
     else:
         F_raxs=data.split_fullset(data.ctr_data_all[condition_raxs][:,0]*0,scale_factors=1, data_type = 'RAXS')
 
@@ -251,56 +251,3 @@ def Sim(data,VARS=vars()):
             bv += VARS['bv_constraint_domain{}'.format(i+1)].cal_distance()
 
     return F,1+bv,fom_scaler
-'''
-def Sim(data,VARS=vars()):
-    F =[]
-    fom_scaler=[]
-    beta=rgh.beta
-
-    #/update_sorbate/begin#
-    for i in range(num_sorbate_slabs):
-        VARS['{}{}'.format(VARS['sorbate_instance_head'],i+1)].set_coordinate_all_rgh()
-    #/update_sorbate/end#
-
-    #normalize the domain weight to make total = 1
-    wt_list = [getattr(rgh_wt, 'wt_domain{}'.format(i+1)) for i in range(num_surface_slabs)]
-    total_wt = sum(wt_list)
-    for i in range(num_surface_slabs):
-        sample.domain['domain{}'.format(i+1)]['wt']=wt_list[i]/total_wt
-
-    #faster solution(a factor of two faster than using loop)
-    #ctr datasets
-    condition_ctr = data.ctr_data_all[:,-1]<100
-    h_, k_, x_,LB_,dL_ = data.ctr_data_all[condition_ctr][:,0], data.ctr_data_all[condition_ctr][:,1], data.ctr_data_all[condition_ctr][:,2],data.ctr_data_all[condition_ctr][:,4],data.ctr_data_all[condition_ctr][:,5]
-    rough_ = (1-beta)/((1-beta)**2 + 4*beta*np.sin(np.pi*(x_-LB_)/dL_)**2)**0.5
-    f_ = rough_*sample.calc_f_all(h_, k_, x_)
-    F_ = abs(f_*f_)
-    #you need to edit the list of extra scaling factor accordingly
-    scaling_factors = [[rgh.scale_nonspecular_rods, rgh.scale_specular_rod][int(each=='specular_rod')] for each in data.scaling_tag]
-    F_ctr = data.split_fullset(F_,scaling_factors)
-
-    #raxs datasets
-    condition_raxs = data.ctr_data_all[:,-1]>=100
-    if True in list(condition_raxs):
-        h_, k_, E_, l_, LB_,dL_ = data.ctr_data_all[condition_raxs][:,0], data.ctr_data_all[condition_raxs][:,1], data.ctr_data_all[condition_raxs][:,2],data.ctr_data_all[condition_raxs][:,3], data.ctr_data_all[condition_raxs][:,4],data.ctr_data_all[condition_raxs][:,5]
-        rough_ = (1-beta)/((1-beta)**2 + 4*beta*np.sin(np.pi*(l_-LB_)/dL_)**2)**0.5
-        f_ = rough_*sample.calc_f_all_RAXS(h_, k_, l_, E_)
-        F_ = abs(f_*f_)
-        #you need to edit the list of extra scaling factor accordingly
-        #scaling_factors = [[rgh.scale_nonspecular_rods, rgh.scale_specular_rod][int(each=='specular_rod')] for each in data.scaling_tag]
-        F_raxs = data.split_fullset(F_,scale_factors=1, data_type = 'RAXS')
-    else:
-        F_raxs = []
-
-    #Now merge both datasets together
-    F = data.merge_datasets(ctr_datasets = F_ctr, raxs_datasets = F_raxs)    
-    fom_scaler = [1]*len(F)
-
-    #calculate bv panelty factor
-    bv = 0
-    if USE_BV:
-        for i in range(num_surface_slabs):
-            bv += VARS['bv_constraint_domain{}'.format(i+1)].cal_distance()
-
-    return F,1+bv,fom_scaler
-'''