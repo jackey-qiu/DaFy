fix a bug relating to mismatch of dim after masking full data; also now the surface slab will store the sorbate domains, an easiser way to build sample instance

EnginePool/models/structure_tools/sxrd_dafy.py
@@ -149,6 +149,7 @@ import pandas as pd
 from scipy.spatial import distance
 from numpy.linalg import inv
 from numba import jit,float64,float32, int32, complex128
+import periodictable as ptab
 import numba
 try:
     from scipy import weave
@@ -2348,7 +2349,8 @@ class Sample:
             oc = np.concatenate((oc,oc_))
             el = np.concatenate((el,el_))
             z=(z+1.)*self.unit_cell.c#z is offseted by 1 unit since such offset is explicitly considered in the calculatino of structure factor
-            f=np.array([el_lib[each] for each in el])
+            f = np.array([getattr(ptab,each).number for each in el])
+            #f=np.array([el_lib[each] for each in el])
             Auc=self.unit_cell.a*self.unit_cell.b*np.sin(self.unit_cell.gamma)
             z_min,z_max=z_min,z_max
             eden=[]
@@ -2394,7 +2396,8 @@ class Sample:
                     eden_layer_water.append(0)
                 if 'layered_sorbate' in slabs[key].keys():
                     if slabs[key]['layered_sorbate']!=[]:
-                        eden_raxs.append(np.sum(el_lib[slabs[key]['layered_sorbate'][0]]*sorbate_density*(2*np.pi*np.array(sigma_layered_sorbate)**2)**-0.5*np.exp(-0.5/np.array(sigma_layered_sorbate)**2*(z_each-np.array(z_layered_sorbate))**2)))
+                        # eden_raxs.append(np.sum(el_lib[slabs[key]['layered_sorbate'][0]]*sorbate_density*(2*np.pi*np.array(sigma_layered_sorbate)**2)**-0.5*np.exp(-0.5/np.array(sigma_layered_sorbate)**2*(z_each-np.array(z_layered_sorbate))**2)))
+                        eden_raxs.append(np.sum(getattr(ptab,slabs[key]['layered_sorbate'][0]).number*sorbate_density*(2*np.pi*np.array(sigma_layered_sorbate)**2)**-0.5*np.exp(-0.5/np.array(sigma_layered_sorbate)**2*(z_each-np.array(z_layered_sorbate))**2)))
                         eden[-1]=eden[-1] + eden_raxs[-1]
                 else:
                     eden_raxs.append(0)
@@ -3515,6 +3518,8 @@ class Slab:
         self.id = np.array([], dtype = np.str)
         self.el = np.array([], dtype = np.str)
         self.T_factor=T_factor
+        #reserve for substrate domain, where you can specify the sorbate domains associating with it
+        self.bound_domains = []
 
         # TODO: Type checking and defaults!
         #self.inst = inst

projects/superrod/standard_scripts/template_script.py
@@ -167,7 +167,7 @@ domains = {}
 for i in range(num_surface_slabs):
     domains['domain{}'.format(i+1)] = {}
     domains['domain{}'.format(i+1)]['slab'] = globals()['surface_{}'.format(i+1)]
-    domains['domain{}'.format(i+1)]['sorbate'] = [globals()['domain_sorbate_{}'.format(j+1)] for j in range(num_sorbate_slabs)]
+    domains['domain{}'.format(i+1)]['sorbate'] = globals()['surface_{}'.format(i+1)].bound_domains 
     domains['domain{}'.format(i+1)]['wt'] = getattr(globals()['rgh_wt'],'wt_domain{}'.format(i+1))
     domains['domain{}'.format(i+1)]['sorbate_sym'] = globals()['sorbate_syms_{}'.format(i+1)]
     domains['domain{}'.format(i+1)]['layered_water'] = rgh_lw
@@ -216,7 +216,8 @@ def Sim(data,VARS=vars(),kwargs = {}):
         dummy_data_ctr = np.array([l_ctr,h_ctr,k_ctr,y_ctr,F_ctr,Ferr_ctr,dL_ctr,LB_ctr]).T
         if 'raxs' in kwargs:
             h_rs, k_rs, l_rs, E_rs = kwargs['raxs'][:,0], kwargs['raxs'][:,1], kwargs['raxs'][:,2],kwargs['raxs'][:,3]
-            f_rs = sample.calc_f_all_RAXS(h_rs, k_rs, l_rs, E_rs)
+            tag = range(len(list(set(zip(h_rs,k_rs,l_rs)))))
+            f_rs = sample.calc_f_all_RAXS(h_rs, k_rs, l_rs, E_rs, tag)
             F_rs = abs(f_rs*f_rs)
             LB_rs = [2]*len(F_rs)
             dL_rs = [2]*len(F_rs)