fix a bug due to misuse of angle unit of radian (supposed to be in degree)

projects/ubmate/XRD_simulator_GUI.py
@@ -1013,8 +1013,8 @@ class MyMainWindow(QMainWindow):
         self.prepare_objects_for_render()
         self.UB_INDEX += 1
         self.ub.newub('current_ub_{}'.format(self.UB_INDEX))
-        bs = self.structures[0].base_structure
-        self.ub.setlat('Triclinic',bs.a,bs.b,bs.c,bs.alpha,bs.beta,bs.gamma)
+        lat_temp = self.structures[0].lattice
+        self.ub.setlat('Triclinic',lat_temp.a,lat_temp.b,lat_temp.c,np.rad2deg(lat_temp.alpha),np.rad2deg(lat_temp.beta),np.rad2deg(lat_temp.gamma))
         self.hardware.settings.hardware.energy = self.energy_keV
         self.dc.setu([[1,0,0],[0,1,0],[0,0,1]])
         self.set_cons()

projects/ubmate/reciprocal_space_plot_v4.py
@@ -116,7 +116,7 @@ class space_plot():
         F = self.lattice.F_many(HKLs, qs)
         I = (F*F.conjugate()).real/I0
 
-        indx = np.where(I > 1e-10)
+        indx = np.where(I > 1e-18)
         Is = I[indx]
         qs = qs[indx]
         HKLs = HKLs[indx]